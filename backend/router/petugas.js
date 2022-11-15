8f860e5f1da299d64da3bb1b9b427bfe44081839
RafiAthallahN <75708409+RafiAthallahN@users.noreply.github.com>
2022-11-15 12:49:18 +0700

    add login petugas

--- backend/router/petugas.js
+++ backend/router/petugas.js
@@ -26,7 +26,7 @@ app.get("/:id", async (res, req) => {
   let param = {
     id_petugas : req.params.id
   }
-  spp.findOne({where:param})
+  petugas.findOne({where:param})
     .then(result => {
       res.json({
         data: result
@@ -39,3 +39,114 @@ app.get("/:id", async (res, req) => {
     })
 })
 
+app.post("/", async (res, req) => {
+  let data = {
+    username: req.body.username,
+    nama: req.body.nama,
+    password: req.body.password,
+    level: req.body.level,
+  }
+  petugas.create(data)
+    .then(result => {
+      res.json({
+        message: "Data Added",
+        data: result
+      })
+    })
+    .catch(error => {
+      res.json({
+        message: error.message
+      })
+    })
+})
+
+app.put("/", async (res, req) => {
+  let param = {
+    id_petugas : req.body.id
+  }
+  let data = {
+    username: req.body.username,
+    nama: req.body.nama,
+    password: req.body.password,
+    level: req.body.level,
+  }
+  petugas.update({ where: param })
+    .then(result => {
+      res.json({
+        message: "Data Updated",
+        data: result
+      })
+    })
+    .catch(error => {
+      res.json({
+        message: error.message
+      })
+    })
+})
+
+app.delete("/:id", async (res, req) => {
+  let param = {
+    id_petugas : req.params.id
+  }
+  petugas.destroy({where:param})
+    .then(result => {
+      res.json({
+        data: result
+      })
+    })
+    .catch(error => {
+      res.json({
+        message: error.message
+      })
+    })
+})
+
+//Login Admin
+app.post("/admin", async (res, req) => {
+  let param ={
+    username : req.params.username,
+    password : md5(req.params.username),
+    level : "admin"
+  }
+  let result = await patugas.findOne({where: param})
+  if (result) {
+    let payload = JSON.stringify(result)
+    //generate token
+    let token = jwt.sign(payload, secret_key)
+    res.json({
+      Logged: true,
+      data: result,
+      token: token
+    })
+  } else {
+    res.json({
+      Logged: false,
+      message: "Invalid Username / Password"
+    })
+  }
+})
+
+//Login petugas
+app.post("/petugas", async (res, req) => {
+  let param ={
+    username : req.params.username,
+    password : md5(req.params.username),
+    level : "petuigas"
+  }
+  let result = await patugas.findOne({where: param})
+  if (result) {
+    let payload = JSON.stringify(result)
+    //generate token
+    let token = jwt.sign(payload, secret_key)
+    res.json({
+      Logged: true,
+      data: result,
+      token: token
+    })
+  } else {
+    res.json({
+      Logged: false,
+      message: "Invalid Username / Password"
+    })
+  }
+})
